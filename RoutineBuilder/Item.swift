322864a2215d071c885d51b6f9fdc8d999de3cfb
SRozhina <sofia.rozhina@gmail.com>
2024-06-19 18:16:13 +0200

    Initial Commit

---
+++ RoutineBuilder/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RoutineBuilder
+//
+//  Created by Sofia Vykhrystyuk on 19/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RoutineBuilderTests/RoutineBuilderTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineBuilderTests.swift
+//  RoutineBuilderTests
+//
+//  Created by Sofia Vykhrystyuk on 19/06/2024.
+//
+
+import Testing
+@testable import RoutineBuilder
+
+struct RoutineBuilderTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
